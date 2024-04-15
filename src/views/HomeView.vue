0af0c5495a80e696cbea8634a860bc257f6800e8
之铭 <zhiming.hao@qq.com>
2024-04-15 14:51:55 +0800

    生产环境打包问题

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,11 @@ import { Viewer } from 'cesium'
 import 'cesium/Build/CesiumUnminified/Widgets/widgets.css'
 
 const viewerDivRef = ref<HTMLDivElement>()
+// 开发环境
 window.CESIUM_BASE_URL = 'node_modules/cesium/Build/CesiumUnminified/'
+// 生产环境
+// 需要将 node_modules/cesium/Build/CesiumUnminified 下所有的静态资源文件放到public/libs/cesium 文件夹下 不然打包后无法运行cesium
+// window.CESIUM_BASE_URL = 'libs/cesium/'
 
 onMounted(() => {
   new Viewer(viewerDivRef.value as HTMLElement)
