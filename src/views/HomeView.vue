5c50550a7e507dce75380dfc3a3f1796f32c167f
之铭 <zhiming.hao@qq.com>
2024-04-15 16:37:33 +0800

    1.隐藏版权信息 2.手动设置初始坐标 3.隐藏原始的cesium原始地球组件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { Viewer } from 'cesium'
+import { onMounted, ref, shallowRef } from 'vue'
+import { Cartesian3, Viewer } from 'cesium'
 import 'cesium/Build/CesiumUnminified/Widgets/widgets.css'
 
-const viewerDivRef = ref<HTMLDivElement>()
+// cesium静态资源处理
 // 开发环境
 window.CESIUM_BASE_URL = 'node_modules/cesium/Build/CesiumUnminified/'
 // 生产环境
 // 需要将 node_modules/cesium/Build/CesiumUnminified 下所有的静态资源文件放到public/libs/cesium 文件夹下 不然打包后无法运行cesium
 // window.CESIUM_BASE_URL = 'libs/cesium/'
 
+// cesium挂载的dom节点
+const viewerDivRef = ref<HTMLDivElement>()
+const viewerRef = shallowRef<Viewer>()
+
+const initialPosition = Cartesian3.fromDegrees(116.391, 39.9163, 2000.0)
+
 onMounted(() => {
-  new Viewer(viewerDivRef.value as HTMLElement)
+  // 初始化地球，并且隐藏原始的cesium配置项
+  viewerRef.value = new Viewer(viewerDivRef.value as HTMLElement, {
+    animation: false, //动画小部件
+    baseLayerPicker: false, //地图图层组件
+    fullscreenButton: false, //全屏组件
+    geocoder: false, //地理编码搜索组件
+    homeButton: false, //首页组件
+    infoBox: false, //信息框
+    sceneModePicker: false, //场景模式
+    selectionIndicator: false, //选取指示器组件
+    timeline: false, //时间轴
+    navigationHelpButton: false, //帮助按钮
+    navigationInstructionsInitiallyVisible: false
+  })
+  // 隐藏版权信息
+  const copyRightEl = viewerRef.value.cesiumWidget.creditContainer as HTMLDivElement
+  copyRightEl.style.display = 'none'
+  // 手动设置初始坐标
+  viewerRef.value.camera.setView({ destination: initialPosition })
+  console.log('cesiums实例对象', viewerRef.value)
 })
 </script>
 
