12fb39a6d7f51d9c775ad9d584c8aa5d71f2057d
之铭 <zhiming.hao@qq.com>
2024-04-15 12:36:57 +0800

    初始化一个地球

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/reset.css'
+
 const app = createApp(App)
+
+declare global {
+  interface Window {
+    CESIUM_BASE_URL: string
+  }
+}
 
 app.use(createPinia())
 app.use(router)
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import { Viewer } from 'cesium'
+import 'cesium/Build/CesiumUnminified/Widgets/widgets.css'
+
+const viewerDivRef = ref<HTMLDivElement>()
+window.CESIUM_BASE_URL = 'node_modules/cesium/Build/CesiumUnminified/'
+
+onMounted(() => {
+  new Viewer(viewerDivRef.value as HTMLElement)
+})
+</script>
 
 <template>
-  <main></main>
+  <main id="cesium-viewer" ref="viewerDivRef"></main>
 </template>
+
+<style scoped lang="scss">
+#cesium-viewer {
+  width: 100%;
+  height: 100%;
+}
+</style>
